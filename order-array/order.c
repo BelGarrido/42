72a0b573297654a1ddd8151adcd103f5755e418f
anaigdx <anaigdx@gmail.com>
2024-11-09 16:57:16 +0100

    Add inert-array and order-array exercises

---
+++ order-array/order.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+	int array[] = {5,7,8,5,34,68,1,3};
+	int size = 8;
+	int i=0;
+	int j;
+	int pivote;
+	int pos_aux;
+	while(i<size){
+		pos_aux = i;
+		j=i+1;
+		while(j<size){
+			if(array[j]<array[pos_aux]){
+				pos_aux = j;
+			}
+			j++;
+		}
+		pivote = array[pos_aux];
+		array[pos_aux] = array[i];
+		array[i] = pivote;
+		i++;
+	}
+	for (int k=0; k<size; k++){
+		printf("%i\n", array[k]);
+	}
+}
+
+//{1|7,8,5,34,68,5,3}
+//{1,3|8,5,34,68,5,7}
+//{1,3,5|8,34,68,5,7}
+//{1,3,5,5|34,68,8,7}
+
+
