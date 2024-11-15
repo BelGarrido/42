ba51be9b7c41c6af5274182b5dc9c052d2bab3b3
anaigdx <anaigdx@gmail.com>
2024-11-15 01:41:28 +0100

    fix strcapitalize

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -22,12 +22,20 @@ char	*ft_strcapitalize(char *str)
 		str[0] = str[0] - 32;
 	}
 	while (str[i] != '\0')
-	{
+	{	
 		if (str[i] == ' ' || str[i] == '+' || str[i] == '-')
 		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
 			{
-				str[i + 1] = str[i + 1] - 32;
+				str[i] = str[i] - 32;
+			}
+		}
+		else
+		{
+			if (i > 0 && str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] + 32;
 			}
 		}
 		i++;
