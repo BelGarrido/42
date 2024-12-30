b1b4fcfd26f11d1cf77243bca24321f0a55f861b
anaigdx <anaigdx@gmail.com>
2024-12-31 00:38:31 +0100

    Modify ft_lstdelone main trying to solve segmentation fault

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -3,7 +3,7 @@
 void	ft_lstdelone (t_list *lst, void (*del)(void *))
 {
 	if (lst != NULL)
-	{
+	{	
 		del(lst -> content);
 		free(lst);
 	}
@@ -16,17 +16,26 @@ void ft_del(void *patatita)
 
 int main (void)
 {
+	printf("UNO");
 	char *number1 = ft_strdup("hola1");
+	t_list *lst1;
+	lst1 = ft_lstnew(number1);
 	char *number2 = ft_strdup("hola2");
+	t_list *lst2;
+	lst2 = ft_lstnew(number2);
 	char *number3 = ft_strdup("hola3");
+	t_list *lst3;
+	lst3 = ft_lstnew(number3);
 	char *number4 = ft_strdup("hola4");
+	t_list *lst4;
+	lst4 = ft_lstnew(number4);
+	printf("HOLA");
 	t_list *ll;
-	ll = ft_lstnew(number1);
-	ll -> next = ft_lstnew(number2);
-	ll -> next -> next = ft_lstnew(number3);
-	ll -> next -> next -> next = ft_lstnew(number4);
-	t_list *elem3 = ll -> next;
+	ll = lst1;
+	ll -> next = lst2;
+	lst2 -> next = lst3;
+	lst3 -> next = lst4;
 	//ft_lstdelone(ll -> next -> next, &ft_del);
-	ft_lstdelone(elem3, &ft_del);
+	ft_lstdelone(lst3, &ft_del);
  	printf("%i\n", ft_lstsize(ll));
 }
